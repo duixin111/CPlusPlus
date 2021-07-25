920a47ed3d89e419bc873e0f9587dae605b734b7
duixin111 <74460899+duixin111@users.noreply.github.com>
2021-07-25 08:45:05 +0800

    斐波那契数列
    描述
    大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0，第1项是1）。
    n\leq 39n≤39
    
    示例1
    输入：
    4
    返回值：
    3

---
+++ practice/Fibonacci.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int Fibonacci(int n) {
+        if(n == 0 || n == 1)
+            return n;
+        int prev = 0;
+        int cur = 1;
+        int value;
+        for(int i = 2; i <= n; i++)
+        {
+            value = prev + cur;
+            prev = cur;
+            cur = value;
+        }
+        return value;
+    }
+};
