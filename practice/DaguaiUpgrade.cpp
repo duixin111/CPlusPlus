518efdebb8b889b9cc327064249628f9637f154a
duixin111 <74460899+duixin111@users.noreply.github.com>
2021-07-30 08:35:54 +0800

    打怪升级
    小易经常沉迷于网络游戏.有一次,他在玩一个打怪升级的游戏,他的角色的初始能力值为a.在接下来的一段时间内,他将会依次遇见n个怪物,每个怪物的防御力为b1,b2,b3...bn.如果遇到的怪物防御力bi小于等于小易的当前能力值c,那么他就能轻松打败怪物,并且使得自己的能力值增加bi;如果bi大于c,那他也能打败怪物,但他的能力值只能增加bi 与c的最大公约数.那么问题来了,在一系列的锻炼后,小易的最终能力值为多少?
    输入描述:
    对于每组数据,第一行是两个整数n (1≤n<100000)表示怪物的数量和a表示小易的初始能力值.
    第二行n个整数, b1,b2. ..bn ( l≤bi<n)表示每个怪物的防御力
    输出描述:
    对于每组数据,输出一行.每行仅包含一个整数,表示小易的最终能力值
    示例1输入输出示例仅供调试，后台判题数据一般不包含示例
    输入
    3 50
    50 105 200 
    5 20
    30 20 15 40 100
    输出
    110 
    205

---
+++ practice/DaguaiUpgrade.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int GreComDiv(int yi,int monster)
+{
+    int z = monster;
+	while(yi % monster != 0)
+	{
+		z = yi % monster;
+		yi = monster;
+		monster = z;	
+	}
+	return z;
+}
+
+int up(int yi, int monster)
+{
+    if(monster <= yi)
+        return monster;
+    return GreComDiv(yi, monster);
+}
+
+int main()
+{
+    int number;
+    int init;
+    while(cin >> number >> init)
+    {
+        vector<int> Def(number);
+        for(int i = 0; i < number; i++)
+        {
+            cin >> Def[i];
+        }
+        for(auto e : Def)
+        {
+            init += up(init, e);
+        }
+        cout << init << endl;
+    }
+    
+    return 0;
+}
