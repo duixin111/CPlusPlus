013f5b54616320fa9d70eddb79fb705a5aa7894a
duixin111 <74460899+duixin111@users.noreply.github.com>
2021-09-09 00:22:27 +0800

    省份数量（BFS）
    有 n 个城市，其中一些彼此相连，另一些没有相连。如果城市 a 与城市 b 直接相连，且城市 b 与城市 c 直接相连，那么城市 a 与城市 c 间接相连。
    
    省份 是一组直接或间接相连的城市，组内不含其他没有相连的城市。
    
    给你一个 n x n 的矩阵 isConnected ，其中 isConnected[i][j] = 1 表示第 i 个城市和第 j 个城市直接相连，而 isConnected[i][j] = 0 表示二者不直接相连。
    返回矩阵中 省份 的数量。

---
+++ practice/findCircleNum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int provinces = isConnected.size();
+        vector<int> visited(provinces);
+        int circles = 0;
+        queue<int> q;
+        for (int i = 0; i < provinces; i++)
+        {
+            if (!visited[i])
+            {
+                q.push(i);
+                while (!q.empty())
+                {
+                    int j = q.front(); q.pop();
+                    visited[j] = 1;
+                    for (int k = 0; k < provinces; k++)
+                    {
+                        if (isConnected[j][k] == 1 && !visited[k])
+                        {
+                            q.push(k);
+                        }
+                    }
+                }
+                circles++;
+            }
+        }
+        return circles;
+    }
+};
