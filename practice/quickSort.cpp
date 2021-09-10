06fc945adde3d8a37942b573af68484cbf43172d
duixin111 <74460899+duixin111@users.noreply.github.com>
2021-09-10 22:12:57 +0800

    快排

---
+++ practice/quickSort.cpp
@@ -0,0 +1,36 @@
+
+void quickSort(int left, int right, vector<int>& arr)
+{
+	if (left >= right)
+		return;
+	int i, j, base, temp;
+	i = left, j = right;
+	base = arr[left];
+
+	while (i < j)
+	{
+		while (arr[j] >= base && i < j)
+			j--;
+		while (arr[i] <= base && i < j)
+			i++;
+		if (i < j)
+		{
+			temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+
+	}
+
+	arr[left] = arr[i];
+	arr[i] = base;
+	quickSort(left, i - 1, arr);
+	quickSort(i + 1, right, arr);
+}
+
+
+
+
+
+
+
